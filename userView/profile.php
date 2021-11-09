ccff6285980f8c5726a1c8ba2584b9969170ac8b
rob2radical <qrobert@rocketmail.com>
2021-11-09 17:51:14 -0600

    working on profile page

--- userView/profile.php
+++ userView/profile.php
@@ -1,19 +1,37 @@
 <?php 
 include_once "header.php";
 include_once "includes/dbh.inc.php";
-$curUser = $_SESSION["userid"]; 
 ?>
 <div class="wrapper">
 
-<section class="signup-form">
-  <h2>User Information<h2>
-  <form action="includes/update.inc.php" method="get">
-      <input type="hidden" id="sessionID" name="sessionID" value="<?php echo $curUser; ?>">
-      <input type="text" name="name" placeholder="Full name...">
-      <input type="text" name="email" placeholder="Email...">
-      <input type="text" name="phone" placeholder="Phone Number...">
-      <input type="text" name="uid" placeholder="Username...">
-    </form>
+<section class="signup-form"> 
+  <h2>User Information<h2> 
+    <?php
+    if(isset($_POST["sessionID"])) 
+    { 
+      $userID = $_POST["sessionID"]; 
+
+      $sql = "SELECT * from users WHERE usersId = ?";
+      $stmt = mysqli_stmt_init($conn);
+      if (!mysqli_stmt_prepare($stmt, $sql)) {
+           header("location: prop.php?error=stmtfailed");
+          exit();
+      } 
+      mysqli_stmt_bind_param($stmt, "s", $userID); 
+      mysqli_stmt_execute($stmt); 
+      // "Get result" returns the results from a prepared statement
+      $result = mysqli_stmt_get_result($stmt);
+      $row = mysqli_fetch_assoc($result);
+      mysqli_stmt_close($stmt);
+
+      $username = $row["usersName"];
+      $useremail = $row["usersEmail"];
+      $usersphone = $row["usersPhone"];
+      $usersuname = $row["usersUid"];
+
+      echo "<h1>" . $username . "</h1>";
+    }
+    ?>
   <h2>Update Information</h2>
   <div class="signup-form-form">
     <form action="includes/update.inc.php" method="post">
