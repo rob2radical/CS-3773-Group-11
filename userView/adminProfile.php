3670ecb3d9190c7234d6940f609cc89d545c7a83
rob2radical <qsy775@my.utsa.edu>
2021-11-10 09:22:10 -0600

    you can now view admin user information

--- userView/adminProfile.php
+++ userView/adminProfile.php
@@ -5,7 +5,36 @@ $curUser = $_SESSION["userid"];
 ?>
 <div class="wrapper">
 
-<section class="signup-form">
+<section class="signup-form"> 
+  <h1> Admin User Information<h1> 
+    <?php 
+    if(isset($_SESSION["userid"])) 
+    { 
+      $userID = $_SESSION["userid"];
+
+      $sql = "SELECT * from users WHERE usersId = ? ";
+      $stmt = mysqli_stmt_init($conn);
+      if(!mysqli_stmt_prepare($stmt, $sql)) { 
+        header("location: adminProfile.php?error=stmtfailed");
+        exit();
+      }
+      mysqli_stmt_bind_param($stmt, "s", $userID);
+      mysqli_stmt_execute($stmt);
+      $result = mysqli_stmt_get_result($stmt);
+      $row = mysqli_fetch_assoc($result);
+      mysqli_stmt_close($stmt);
+
+      $username = $row["usersName"];
+      $useremail = $row["usersEmail"];
+      $usersphone = $row["usersPhone"];
+      $usersuname = $row["usersUid"];
+
+      echo "<h2>" . $username . "</h2>";
+      echo "<h2>" . $useremail . "</h2>";
+      echo "<h2>" . $usersphone . "</h2>";
+      echo "<h2>" . $usersuname . "</h2>";
+    }
+    ?>
   <h2>Update Account</h2>
   <div class="signup-form-form">
     <form action="includes/update.inc.php" method="post">
