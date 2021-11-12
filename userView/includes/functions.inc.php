d8e564efb3b04a646bf42d27229cc7132baf45b5
rob2radical <qrobert@rocketmail.com>
2021-11-11 20:46:05 -0600

    update user info input handling

--- userView/includes/functions.inc.php
+++ userView/includes/functions.inc.php
@@ -156,7 +156,54 @@ function uidExists($conn, $username) {
 		$result = false;
 		return $result;
 	}
+	mysqli_stmt_close($stmt);
+}
 
+function phoneExists($conn, $phone) { 
+	$sql = "SELECT * FROM users WHERE usersPhone = ?;";
+	$stmt = mysqli_stmt_init($conn);
+	if(!mysqli_stmt_prepare($stmt, $sql)) 
+	{ 
+		header("location: ../userSignUp.php?error=stmtfailed");
+		exit();
+	}
+
+	mysqli_stmt_bind_param($stmt, "s", $phone);
+	mysqli_stmt_execute($stmt);
+
+	$resultData = mysqli_stmt_get_result($stmt);
+
+	if($row = mysqli_fetch_assoc($resultData)) { 
+		return $row;
+	}
+	else { 
+		$result = false;
+		return $result;
+	} 
+	mysqli_stmt_close($stmt);
+}
+
+function emailExists($conn, $email) { 
+	$sql = "SELECT * FROM users WHERE usersEmail = ?;";
+	$stmt = mysqli_stmt_init($conn);
+	if(!mysqli_stmt_prepare($stmt, $sql)) 
+	{ 
+		header("location: ../userSignUp.php?error=stmtfailed");
+		exit();
+	}
+
+	mysqli_stmt_bind_param($stmt, "s", $email);
+	mysqli_stmt_execute($stmt);
+
+	$resultData = mysqli_stmt_get_result($stmt);
+
+	if($row = mysqli_fetch_assoc($resultData)) { 
+		return $row;
+	}
+	else { 
+		$result = false;
+		return $result;
+	}
 	mysqli_stmt_close($stmt);
 }
 
@@ -211,7 +258,6 @@ function createUser($conn, $name, $email, $phone, $username, $pwd) {
 // Update new user in the existing database
 function updateUser($conn, $name, $email, $phone, $username, $userId, $role)
 {
-		#$userId = $_SESSION["userid"];
 		$sql = "SELECT * FROM users WHERE usersId = ?"; 
 		$stmt = mysqli_stmt_init($conn);
 		if (!mysqli_stmt_prepare($stmt, $sql)) {
--- userView/includes/update.inc.php
+++ userView/includes/update.inc.php
@@ -4,26 +4,61 @@ require_once "functions.inc.php";
 
 $userId = $_POST["sessionID"];
 $userRole = $_POST["isAdmin"];
-if(isset($_POST["update"])) {
-
+if(isset($_POST["update"])) 
+{
     $name = $_POST["name"]; 
     $email = $_POST["email"]; 
     $phone = $_POST["phone"]; 
     $uid = $_POST["uid"]; 
-
-    updateUser($conn, $name, $email, $phone, $uid, $userId, $userRole);
-} 
-else {
-    if($userRole == 1)
-    {
-        header("location: ../adminProfile.php?error=updateerror");
-        exit();
-    }
-    else
-    {
-        header("location: ../profile.php?error=updateerror");
-        exit();
-    }
+    
+    if(!uidExists($conn, $uid, $email)) 
+    { 
+        if(!phoneExists($conn, $phone)) 
+        { 
+            if(!emailExists($conn, $email)) 
+            { 
+                updateUser($conn, $name, $email, $phone, $uid, $userId, $userRole);
+            } 
+            else 
+            { 
+                if($userRole == 1) 
+                {
+                    header("location: ../adminProfile.php?error=emailExistserror");
+                    exit();
+                }
+                else
+                {
+                    header("location: ../profile.php?error=emailExistserror");
+                    exit();
+                } 
+            } 
+        } 
+        else 
+        { 
+            if($userRole == 1) 
+            { 
+                header("location: ../adminProfile.php?error=phoneExistserror"); 
+                exit(); 
+            } 
+            else 
+            { 
+                header("location: ../profile.php?error=phoneExistserror"); 
+                exit(); 
+            } 
+        }
+    } 
+    else 
+    { 
+        if($userRole == 1) 
+        { 
+            header("location: ../adminProfile.php?error=uidExistserror");
+            exit();
+        }
+        else
+        { 
+            header("location: ../profile.php?error=uidExistserror");
+            exit();
+        } 
+    } 
 }
-
 ?>
