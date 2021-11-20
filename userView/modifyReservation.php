eb9ddb67424302e5497e31193890a140c5661350
rob2radical <qrobert@rocketmail.com>
2021-11-19 20:10:02 -0600

    still working on update

--- userView/modifyReservation.php
+++ userView/modifyReservation.php
@@ -1,26 +1,99 @@
 <?php
-  include_once 'header.php';
+  session_start();
+  include_once 'includes/functions.inc.php';
+  include_once 'includes/dbh.inc.php';
 ?>
-<div class="wrapper"> 
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>View Properties - Hotel Miago</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+    <!--A quick navigation-->
+    <nav>
+      <div class="wrapper">
+        <a href="index.php"><img src="img/Miago Logo.jpg" width="1000px"alt="Miago logo"></a>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="ViewProps.php">View Properties</a></li>
+          <?php
+            if (isset($_SESSION["useruid"]) && ($_SESSION["isAdmin"] == 0)) {
+              echo "<li><a href='profile.php'>Profile Page</a></li>";
+              echo "<li><a href='reserve.php'>Make a Reservation</a></li>";
+              echo "<li><a href='logout.php'>Logout</a></li>";
+            }
+            else if(isset($_SESSION["useruid"]) && ($_SESSION["isAdmin"] == 1))
+            {
+              echo "<li><a href='adminProfile.php'>Admin Page</a></li>";
+              echo "<li><a href='logout.php'>Logout</a></li>";
+            }
+            else {
+              echo "<li><a href='userSignUp.php'>Sign up</a></li>";
+              echo "<li><a href='userLogin.php'>Log in</a></li>";
+            }
+          ?>
+        </ul>
+      </div>
+    </nav>
+
+<div class="wrapper">
   <section class="signup-form"> 
     <h1>Update Reservation Information</h1> 
     <div class="signup-form-form"> 
       <form action="includes/updateReserve.inc.php" method="post"> 
           <?php 
-          if(isset($_POST["sessionID"])) 
+          if(isset($_POST["updateReserve"])) 
           { 
-            $resID = $_POST["sessionID"];
+            $resID = $_POST["res"];
           }
-          else if(isset($_GET["resId"])) 
+          else 
           { 
-            $resID = $_GET["resId"];
+            echo "error";
           }
-          ?> 
-          <label for="property">Property:</label>
-          <select name="property" id="property">
-          <input type="hidden" id="sessionID" name="sessionID">
-          <label for="room_type">Room Type:</label>
-          <select name="room_type" id="room_type">
+          $sql = "SELECT hotels.numRoomS, hotels.numRoomQ, hotels.numRoomK FROM hotels JOIN reservations ON hotels.hotelName = reservations.hotelName WHERE resId = ? "; 
+          $stmt = mysqli_stmt_init($conn); 
+          if(!mysqli_stmt_prepare($stmt, $sql)) { 
+            header("location: modifyReservation.php?error=stmtfailed");
+            exit(); 
+          } 
+          mysqli_stmt_bind_param($stmt, "s", $resID); 
+          mysqli_stmt_execute($stmt); 
+          // "Get result" returns the results from a prepared statement 
+          $result = mysqli_stmt_get_result($stmt);
+          $row = mysqli_fetch_assoc($result);
+          mysqli_stmt_close($stmt);
+          if($row["numRoomS"] == 0 && $row["numRoomQ"] == 0 && $row["numRoomK"] == 0) 
+          { 
+            echo "No rooms available";
+            exit();
+          }
+          else 
+          { 
+            echo "<label for=room_type>Select Room Type:</label>";
+            echo "<select name=room_type id=room_type>";
+            if($row["numRoomS"] != NULL)
+            {  
+              echo "<option>Standard</option>";
+            } 
+            if($row["numRoomQ"] != NULL) 
+            {  
+              echo "<option>Queen</option>";
+            } 
+            if($row["numRoomK"] != NULL) 
+            { 
+              echo "<option>King</option>";
+            } 
+          }
+          echo "</select>";
+          ?>
+          <input type="hidden" id="sessionID" name="sessionID" value="<?php echo $resID;?>">
+          <input type="hidden" id="resid" name="resid" value="<?php echo $resID;?>">
           <label for="check-in">Check-In Date:</label>
           <input type="date" id="check-in" name="check-in">
           <label for="check-out">Check-Out Date:</label>
